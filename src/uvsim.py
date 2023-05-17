b566a5aa0b1b2f06b116e52988824b4536dda356
Robby Stohel <robert.stohel@anyonehome.com>
2023-05-17 09:18:02 -0600

    Set repo foundational structure

---
+++ src/uvsim.py
@@ -0,0 +1,48 @@
+"""UVSim.
+
+DOCUMENT
+"""
+
+#TODO: Library imports
+
+
+#TODO: Set ACCU and MEM as global variables
+
+
+#TODO: Function to handle/return terminal input
+
+#TODO: Function to handle/return terminal input
+
+#TODO: Function to handle output to terminal
+
+#TODO: Function to handle/return input from memory space
+
+#TODO: Function to handle output to memory space
+
+#TODO: Function to handle binary addition
+
+#TODO: Function to handle binary subtraction
+
+#TODO: Function to handle binary multiplication
+
+#TODO: Function to handle binary division
+
+
+def main():
+    """Main script driver."""
+    #TODO: Validate request
+
+    #TODO: Determine operation
+
+    #TODO: ...
+
+    #TODO: ...
+
+    #TODO: Return result
+
+    pass
+
+if __name__ == "__main__":
+    #TODO: Handle terminal calls
+
+    main()
