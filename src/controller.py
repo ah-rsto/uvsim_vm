6d1425745d88bb9466f6953fe03015c6e219fea3
Taylie Whitworth <105079720+tayliewhitworth@users.noreply.github.com>
2023-06-20 08:07:30 -0600

    Add files via upload

---
+++ src/controller.py
@@ -0,0 +1,207 @@
+"""Controller
+
+This module manages the controller components.
+"""
+
+from model import DataModel
+
+
+class ArithmeticController:
+    """Arithmetic Controller Class Docstring Text."""
+
+    def addition(self, accumulator, registers, register_idx):
+        """Addition Method Docstring Text."""
+        return accumulator + registers[register_idx]
+
+    def subtraction(self, accumulator, registers, register_idx):
+        """Subtration Method Docstring Text."""
+        return accumulator - registers[register_idx]
+
+    def multiplication(self, accumulator, registers, register_idx):
+        """Multiplication Method Docstring Text."""
+        if not isinstance(registers[register_idx], (int, float)):
+            raise ValueError("Invalid register_idx: must be a number")
+        result = accumulator * registers[register_idx]
+        return result % 10000
+
+    def division(self, accumulator, registers, register_idx):
+        """Division Method Docstring Text."""
+        if not isinstance(registers[register_idx], (int, float)):
+            raise ValueError("Invalid register_idx: must be a number")
+        if registers[register_idx] == 00:
+            raise ValueError("Invalid register_idx: Cannot divide by 0")
+        else:
+            return (accumulator // registers[register_idx]) % 10000
+
+
+class BranchController:
+    """Branch Controller Class Docstring Text."""
+
+    def branch(self, register_idx):
+        """Branch Method Docstring Text."""
+        if -2 < register_idx and register_idx < 100:
+            cursor = register_idx - 1
+        else:
+            raise IndexError(f"Memory index '{register_idx}' not in range.")
+        return cursor
+
+    def branch_zero(self, cursor, accumulator, register_idx):
+        """Branch Zero Method Docstring Text."""
+        if accumulator == 0:
+            if -2 < register_idx and register_idx < 100:
+                cursor = register_idx - 1
+            else:
+                raise IndexError(f"Memory index '{register_idx}' not in range.")
+        return cursor
+
+    def branch_negative(self, cursor, accumulator, register_idx):
+        """Branch Negative Method Docstring Text."""
+        if accumulator < 0:
+            if -2 < register_idx and register_idx < 100:
+                cursor = register_idx - 1
+            else:
+                raise IndexError(f"Memory index '{register_idx}' not in range.")
+        return cursor
+
+    def halt(self, halted, register_idx):
+        """Halt Method Docstring Text."""
+        cursor = register_idx
+        if halted:
+            halted()
+            return cursor
+        else:
+            print("Program Completed")
+            return cursor
+
+
+class UVSimController(ArithmeticController, BranchController):
+    """App Controller Class Docstring Text."""
+
+    def __init__(self, halted, display_values=None):
+        """"""
+        super().__init__()
+        self.data_model = DataModel()
+        self.display_values = display_values
+        self.cursor = 0
+        self.instruction = 0
+        self.halted = halted
+
+    def reset_accumulator(self) -> None:
+        """Requests data model accumulator reset.
+
+        :param: None
+        :return: None
+        """
+        self.data_model.reset_accumulator()
+
+    def reset_cursor(self) -> None:
+        """Resets position in instruction set runtime
+
+        :param: None
+        :return: None
+        """
+        self.cursor = 0
+
+    def reset_instruction(self) -> None:
+        """Resets initial instruction assignment
+
+        :param: None
+        :return: None
+        """
+        self.instruction = 0
+
+    def load_program(self, filename):
+        self.data_model.load_program(filename)
+
+    def get_program_text(self):
+        program_text = ""
+        registers = self.data_model.get_instructions()
+
+        for i, val in enumerate(registers):
+            idx, val = str(i).rjust(2, "0"), (
+                val if "-" in str(val) else "+" + str(val)
+            )
+            program_text += f"{idx}:   {val}\n"
+        return program_text
+
+    def get_acc_cur(self):
+        return f"{self.data_model.get_accumulator()}\n", f"{self.cursor}\n"
+
+    def execute_program(self, read_from_user, write_to_console):
+        """Execute Method Docstring Text."""
+        while True:
+            self.instruction = self.data_model.get_instruction(self.cursor)
+            operation_code = abs(self.instruction) // 100
+            instruction_idx = abs(self.instruction) % 100
+
+            if self.display_values:
+                self.display_values(
+                    f"{self.data_model.get_accumulator()}\n", f"{self.cursor}\n"
+                )
+
+            match operation_code:
+                case 10:
+                    self.data_model.set_instruction(instruction_idx, read_from_user())
+                case 11:
+                    write_to_console(self.data_model.get_instruction(instruction_idx))
+                case 20:
+                    self.data_model.set_accumulator(
+                        self.data_model.get_instruction(instruction_idx)
+                    )
+                case 21:
+                    self.data_model.set_instruction(
+                        instruction_idx, self.data_model.get_accumulator()
+                    )
+                case 30:
+                    self.data_model.set_accumulator(
+                        self.addition(
+                            self.data_model.get_accumulator(),
+                            self.data_model.get_instructions(),
+                            instruction_idx,
+                        )
+                    )
+                case 31:
+                    self.data_model.set_accumulator(
+                        self.subtraction(
+                            self.data_model.get_accumulator(),
+                            self.data_model.get_instructions(),
+                            instruction_idx,
+                        )
+                    )
+                case 32:
+                    self.data_model.set_accumulator(
+                        self.division(
+                            self.data_model.get_accumulator(),
+                            self.data_model.get_instructions(),
+                            instruction_idx,
+                        )
+                    )
+                case 33:
+                    self.data_model.set_accumulator(
+                        self.multiplication(
+                            self.data_model.get_accumulator(),
+                            self.data_model.get_instructions(),
+                            instruction_idx,
+                        )
+                    )
+                case 40:
+                    self.cursor = self.branch(instruction_idx, self.cursor)
+                case 41:
+                    self.cursor = self.branch_negative(
+                        self.cursor, self.data_model.get_accumulator(), instruction_idx
+                    )
+                case 42:
+                    self.cursor = self.branch_zero(
+                        self.cursor, self.data_model.get_accumulator(), instruction_idx
+                    )
+                case 43:  # HALT
+                    self.cursor = self.halt(self.halted, instruction_idx)
+                    break
+                case _:
+                    print(
+                        f"Invalid operation code '{operation_code}'. \n"
+                        "Program terminated"
+                    )
+                    break
+
+            self.cursor += 1
---
+++ src/view.py
@@ -0,0 +1,174 @@
+"""View
+
+This module manages the view model.
+"""
+
+import tkinter as tk
+from controller import UVSimController
+from tkinter import filedialog
+import customtkinter
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
+
+
+class UVSimGUI(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.uvsim = UVSimController(self.halted, self.display_values)
+
+        self.title("UVSim")
+        self.geometry("780x620")
+
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=250, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+        self.header = customtkinter.CTkLabel(
+            self.sidebar_frame,
+            text="UVSim GUI",
+            font=customtkinter.CTkFont(size=20, weight="bold"),
+        )
+        self.header.grid(row=0, column=0, padx=20, pady=(20, 10))
+
+        self.accumulator_label = customtkinter.CTkTextbox(
+            self.sidebar_frame,
+            width=200,
+            height=150,
+            font=customtkinter.CTkFont(size=12, weight="normal"),
+        )
+        self.accumulator_label.grid(row=1, column=0, padx=20, pady=10)
+        self.accumulator_label.insert(tk.INSERT, "Accumulator:\n")
+
+        self.cursor = customtkinter.CTkTextbox(
+            self.sidebar_frame,
+            width=200,
+            height=150,
+            font=customtkinter.CTkFont(size=12, weight="normal"),
+        )
+        self.cursor.grid(row=2, column=0, padx=20, pady=10)
+        self.cursor.insert(tk.INSERT, "Counter:\n")
+
+        self.console_output = customtkinter.CTkTextbox(
+            self.sidebar_frame,
+            width=200,
+            height=150,
+            font=customtkinter.CTkFont(size=12, weight="normal"),
+        )
+        self.console_output.grid(row=3, column=0, padx=20, pady=10)
+        self.console_output.insert(tk.INSERT, "Console Output:\n")
+
+        self.upload_btn = customtkinter.CTkButton(
+            self.sidebar_frame, text="Upload BasicML file", command=self.open_program
+        )
+        self.upload_btn.grid(row=5, column=0, padx=20, pady=10)
+
+        self.program_text = customtkinter.CTkTextbox(self, width=300, height=536)
+        self.program_text.grid(
+            row=0, column=1, rowspan=2, padx=20, pady=(20, 0), sticky="nsew"
+        )
+        self.program_text.insert(tk.INSERT, "BasicML program will be displayed here.")
+
+        self.execute_btn = customtkinter.CTkButton(
+            self, text="Run File", command=self.execute_program
+        )
+        self.execute_btn.grid(row=3, column=1, padx=20, pady=10, sticky="nsew")
+
+    def reset_textbox(self, textbox, text):
+        textbox.delete("1.0", tk.END)
+        textbox.insert(tk.INSERT, text)
+
+    def reset_textboxes(self):
+        self.reset_textbox(self.accumulator_label, "Accumulator:\n")
+        self.reset_textbox(self.cursor, "Counter:\n")
+        self.reset_textbox(self.console_output, "Console output:\n")
+
+    def read_from_user(self):
+        dialog = customtkinter.CTkInputDialog(
+            text="Enter a number from -9999 to 9999", title="READ Input"
+        )
+        user_input = dialog.get_input()
+
+        try:
+            value = int(user_input)
+            if value < -9999 or value > 9999:
+                raise ValueError
+            return value
+        except ValueError:
+            self.write_to_console("Invalid input. Try again.")
+            return self.read_from_user()
+
+    def write_to_console(self, value):
+        self.console_output.insert(tk.END, str(value) + "\n")
+        self.console_output.see(tk.END)
+
+    def update_program(self):
+        self.program_text.delete("1.0", tk.END)
+        program_text = "Memory Registers:\n"
+        program_text += self.uvsim.get_program_text()
+
+        self.program_text.insert(tk.INSERT, program_text)
+
+    # def open_program(self):
+    #     # self.program_text.delete("1.0", tk.END)
+    #     self.filename = filedialog.askopenfilename(
+    #         initialdir="/",
+    #         title="Select file",
+    #         filetypes=(("txt files", "*.txt"), ("all files", "*.*")),
+    #     )
+    #     if self.filename == "":
+    #         self.program_text.insert(tk.INSERT, "No file selected. Try again.")
+    #     self.uvsim.load_program(self.filename)
+    #     self.update_program()
+    #     self.reset_textboxes()
+    #     # self.execute_program()
+    def open_program(self, filename: str = "") -> None:
+        """Requests program instruction set and displays for user.
+
+        :param filename: String containing file path
+        :return: None
+        """
+        self.filename = filename
+        while self.filename == "":
+            self.program_text.delete("1.0", tk.END)
+            self.filename = filedialog.askopenfilename(
+                initialdir="/",
+                title="Select file",
+                filetypes=(("txt files", "*.txt"), ("all files", "*.*")),
+            )
+            if self.filename == "":
+                self.program_text.insert(tk.INSERT, "No file selected. Try again.")
+        self.uvsim.load_program(self.filename)
+        self.update_program()
+        self.reset_textboxes()
+        # self.execute_program()
+
+    def execute_program(self):
+        # self.update_program()
+        # new lines added
+        self.uvsim.reset_accumulator()
+        self.uvsim.reset_cursor()
+        self.uvsim.reset_instruction()
+        self.open_program(filename=self.filename)
+
+        # end of new lines added
+        self.reset_textboxes()
+        self.uvsim.execute_program(self.read_from_user, self.write_to_console)
+
+    def display_values(self, accumulator, counter):
+        accumulator, cursor = self.uvsim.get_acc_cur()
+
+        self.accumulator_label.insert(tk.END, str(accumulator))
+        self.cursor.insert(tk.END, str(cursor))
+
+    def halted(self):
+        self.write_to_console(
+            "Program completed.\n\nTo run another file, click 'Upload BasicML file'"
+        )
+
+
+app = UVSimGUI()
+app.mainloop()
